c23090417b93a189d6881de0c22f83f37099c8fd
Perchant76 <63096336+Perchant76@users.noreply.github.com>
2022-06-05 13:38:43 +0200

    Add files via upload

--- first-install.sh
+++ first-install.sh
@@ -10,12 +10,18 @@ echo sudo pacman -S blackarch-exploitation
 echo sudo pacman -S blackarch-defensive
 echo sudo pacman -S blackarch-wireless
 echo sudo pacman -S blackarch-reversing
+echo sudo pacman -S yay
 echo y | sudo pacman -S aura
-echo y | sudo aura -S snap yay openvpn terminator flameshot irssi hexchat visual-studio-code-bin keepassxc
+echo y | sudo aura -S snap yay openvpn terminator flameshot irssi hexchat visual-studio-code-bin keepassxc thunderbird
 echo y | sudo snap install spotify termius
 echo curl -s 'https://download.opensuse.org/repositories/home:/ungoogled_chromium/Arch/x86_64/home_ungoogled_chromium_Arch.key' | sudo pacman-key -a -
-echo '
+echo "'
 [home_ungoogled_chromium_Arch]
 SigLevel = Required TrustAll
 Server = https://download.opensuse.org/repositories/home:/ungoogled_chromium/Arch/$arch' | sudo tee --append /etc/pacman.conf
+yay -S openasar-git"
+echo yay -S vmware-workstation
+echo sudo aura -S slack
+echo snap install ticktick
 echo sudo aura -Syu
+echo "done :)"
